326971c685e2a251fe55b146e91200f7c82ce84c
andreacolina <a.colina.2020@alumnos.urjc.es>
2021-01-23 16:33:33 +0100

    Refactor

--- Node/test/unitary/__mocks__/aws-sdk.js
+++ Node/test/unitary/__mocks__/aws-sdk.js
@@ -1,6 +1,3 @@
 const AWS = require('aws-sdk');
-const putFn = ((film, cb) => cb(null, film));
-const films = { Items : [{ name: 'Los 101 Dalmatas', year: '1957'},{ name: 'El Rey Leon', year: '1998'}]};
-const scanFn = ((_, cb) => cb(null, films));
-AWS.DynamoDB.DocumentClient = jest.fn().mockReturnValue({ put: putFn, scan: scanFn});
+
 module.exports = AWS;
