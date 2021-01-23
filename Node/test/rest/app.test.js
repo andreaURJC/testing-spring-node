0e99be40d3792223fb8e2ecf31ab073a6b774eb2
Juanma <jmgs15@github.com>
2021-01-23 20:50:35 +0100

    Node: fixed container

--- Node/test/rest/app.test.js
+++ Node/test/rest/app.test.js
@@ -3,26 +3,38 @@ const supertest = require('supertest');
 const request = supertest(app);
 const AWS = require('aws-sdk');
 const { GenericContainer } = require("testcontainers");
-const {createTable,deleteTable} = require('../../src/db/createTable')
+const createTableIfNotExist = require("../../src/db/createTable")
+
+let dynamoContainer;
 
 beforeAll(async () => {
     dynamoContainer = await new GenericContainer("amazon/dynamodb-local", "1.13.6")
         .withExposedPorts(8000)
         .start();
 
-        (async() => { 
-            await createTableIfNotExist("films");
-          })();
+    AWS.config.update({
+      region: "local",
+      endpoint: process.env.AWS_DYNAMO_ENDPOINT || `http://localhost:${dynamoContainer.getMappedPort(8000)}`,
+      accessKeyId: "xxxxxx", // No es necesario poner nada aquí
+      secretAccessKey: "xxxxxx", // No es necesario poner nada aquí
+    });
+
+    // CREATE DYNAMODB TABLE ONLY IF NOT EXIST PREVIOUSLY
+    await createTableIfNotExist("films");
+
+    console.log(dynamoContainer)
+
 });
 
+
+
 afterAll(async () => {
-    await deleteTable();
+    await dynamoContainer.stop();
 });
 
 const film = { name: 'Dumbo', year: '1997' }
 
 test("Given user, when he creates new film, then the film is saved.", async () => {
-    putFn(AWS);
 
     const response = await request.post('/api/films/')
         .send(film)
@@ -33,7 +45,6 @@ test("Given user, when he creates new film, then the film is saved.", async () =
 })
 
 test("Given user, when he gets all films, then should return all films.", async () => {
-    scanFn(AWS);
 
     const response = await request.get('/api/films/')
         .expect(200)
