2753dab792e423133a52bd02a85e23f6bcac7467
andreacolina <a.colina.2020@alumnos.urjc.es>
2021-01-23 10:28:35 +0100

    Added test module

---
+++ Node/src/app.test.js
@@ -0,0 +1,9 @@
+const app = require('../server.js');
+const supertest = require('supertest');
+
+const request = supertest(app);
+
+test("Given user, when he creates new film, then the films is saved.", async() => {
+
+
+})
