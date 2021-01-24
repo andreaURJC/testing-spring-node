2db102f5cb0b828fcffbf806b9b69234558de6bf
Juanma <jmgs15@github.com>
2021-01-24 12:32:52 +0100

    Node: Fixed unitary tests

--- Node/test/unitary/app.test.js
+++ Node/test/unitary/app.test.js
@@ -2,12 +2,22 @@ const app = require('../../src/app');
 const supertest = require('supertest');
 const request = supertest(app);
 const AWS = require('aws-sdk');
-const { putFnError, putFn, scanFn } = require('./mockdb')
+
+jest.mock('aws-sdk')
+
+const setDocumentClientAWS = mockedImplementation => {
+  AWS.DynamoDB.DocumentClient.mockImplementation(() => mockedImplementation)
+}
 
 const film = { name: 'Dumbo', year: '1997' }
 describe("Test create a new film: ", () => {
   test("Given user, when he creates new film, then the film is saved.", async () => {
-    putFn(AWS);
+
+    const documentClient = {
+      put: (params, cb) => cb(null, film)
+    }
+
+    setDocumentClientAWS(documentClient)
 
     const response = await request.post('/api/films/')
       .send(film)
@@ -18,18 +28,29 @@ describe("Test create a new film: ", () => {
   })
 
   test("Given user, when he creates new film, then return error.", async () => {
-    putFnError(AWS);
+    
+    const documentClient = {
+      put: (params, cb) => cb(new Error(), null)
+    }
+
+    setDocumentClientAWS(documentClient)
 
     const response = await request.post('/api/films/')
       .send(film)
       .expect(400)
 
   })
-});
+})
 
 describe("Get all films: ", () => {
   test("Given user, when he gets all films, then should return all films.", async () => {
-    scanFn(AWS);
+
+    const films = { Items: [{ name: 'Los 101 Dalmatas', year: '1957' }, { name: 'El Rey Leon', year: '1998' }] };
+    const documentClient = {
+      scan: (params, cb) => cb(null, films)
+    }
+
+    setDocumentClientAWS(documentClient)
 
     const response = await request.get('/api/films/')
       .expect(200)
